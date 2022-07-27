31929880128e0bbf53dace90eac063bed8499066
Rawan Albejadi <108380243+Revnxx@users.noreply.github.com>
2022-07-27 22:59:43 +0300

    Update tremendous_hillar_turing1.ino

--- tremendous_hillar_turing1.ino
+++ tremendous_hillar_turing1.ino
@@ -1,22 +1,77 @@
 #include <Servo.h>
+Servo gripper;
+Servo wrist;
+Servo elbow;
+Servo shoulder;
+Servo base;
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+double base_angle=90;
+double shoulder_angle=90;
+double elbow_angle=90;
+double wrist_angle=90;
 
-int pos = 0;    // variable to store the servo position
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+ Serial.begin(115200);
+   base.attach(8);
+  shoulder.attach(9); 
+  elbow.attach(10);
+  wrist.attach(11);
+  gripper.attach(12); 
+
+  base.write(base_angle);
+  shoulder.write(shoulder_angle);
+  elbow.write(elbow_angle);
+  wrist.write(wrist_angle);
+
+}
+
+
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  
+  String computerText = Serial.readStringUntil('@');
+  computerText.trim();
+  if (computerText.length() == 0) {
+    return;
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+  // 92-0-130
+  String command = getValue(computerText, ' ',0);
+
+    if (command == "right" || command == "رايت" || command == "Right") {
+      base.write(base_angle -= 20);
+    }
+    if (command == "left" || command == "Left" || command == "لفت") {
+     base.write(base_angle += 20);
+    }
+
+    if (command == "top" || command == "توب" || command == "Top") {
+      shoulder.write(shoulder_angle -= 20);
+    }
+
+   if (command == "bottom"|| command == "بوتوم" || command == "Bottom") {
+     shoulder.write(shoulder_angle += 20);
+    }
+    Serial.println(command);
+  Serial.println("WORKING");
+  delay(1000);
+
 }
+
